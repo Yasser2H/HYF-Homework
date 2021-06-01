232ce5095fa421aad0d957dcbd34e927b70497fd
Yasser <Yasser.H@outlook.dk>
2021-06-01 19:48:16 +0200

    Bug Fixes

--- javascript/javascript1/week4/script.js
+++ javascript/javascript1/week4/script.js
@@ -3,7 +3,7 @@ let personName, nameIsSaved = false,
 
 function getReply(command) {
   processedText = processText(command);
-//  console.log(processedText);
+  //  console.log(processedText);
   if (processedText.substring(0, 13) == "hellomynameis" && !nameIsSaved) {
     nameIsSaved = true;
     personName = command.substring(17, command.length);
@@ -12,17 +12,25 @@ function getReply(command) {
     return ("You already provided your name! Aren't you " + personName + "?");
   } else if (processedText.substring(0, 3) == "add") {
     todoList.push(command.substring(4, command.length - 11));
-//    console.log(todoList);
     return (command.substring(4, command.length - 11) + " added to your todo");
   } else if (processedText.substring(0, 6) == "remove") {
     todoList = todoList.filter(e => e !== command.substring(7, command.length - 13));
     return (command.substring(7, command.length - 13) + " removed from your todo");
+  } else if (processedText.includes("+") || processedText.includes("-") || processedText.includes("*") || processedText.includes("/")) {
+    let math = processedText.replace("whatis", "");
+    math = math.replace("?", "");
+    return (math + " = " + eval(math));
+  } else if (processedText.substring(4, 9) == "timer") {
+    let timerMin = parseInt(processedText.substring(12, 14), 10);
+    startTimer(timerMin);
+    return ("Timer: is set for " + timerMin + " minutes");
+  } else if (processedText.includes("quote")) {
+    quoteOfTheDay()
+    console.log("quote is found");
   } else {
     return processCommand(processedText);
   }
 }
-
-// Remove fishing from my todo
 
 function processText(rawText) {
   let text = rawText.replace(/\s/g, "");
@@ -30,24 +38,58 @@ function processText(rawText) {
   return text;
 }
 
+function startTimer(min) {
+  var sec = min * 60;
+  var timer = setInterval(function() {
+    sec--;
+    if (sec < 0) {
+      clearInterval(timer);
+      console.log("Timer: the time is up!");
+    }
+  }, 1000);
+}
+
+function quoteOfTheDay() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      let myObj = JSON.parse(this.responseText);
+      alert(myObj.name);
+      console.log(myObj.name);
+    }
+  };
+  xmlhttp.open("GET", "https://www.w3schools.com/js/json_demo.txt", true);
+  xmlhttp.send();
+
+}
 
 function processCommand(cleanText) {
   switch (cleanText) {
     case "whatismyname?":
       response = "Your name is " + personName + "!";
       break;
-    case "whatismyname":
-      response = "My name's Siri.";
+    case "whatisonmytodo?": // "you have 2 todos - fishing and singing in the shower"
+      let todoListItems = "";
+      for (var i = 0; i < todoList.length - 1; i++) {
+        todoListItems += todoList[i];
+        todoListItems += " and ";
+      }
+      todoListItems += todoList[i];
+      response = "You have " + todoList.length + " items in your todo list - " + todoListItems;
       break;
-    case "howareyou":
-      response = "I'm good.";
+    case "whatdayisittoday?": // "E.g. if today is 30/8/2019 then it should respond with 30. of August 2019"
+      const date = new Date(); // 2009-11-10
+      const year = date.toLocaleString('default', {
+        year: 'numeric'
+      });
+      const month = date.toLocaleString('default', {
+        month: 'long'
+      });
+      const day = date.toLocaleString('default', {
+        day: 'numeric'
+      });
+      response = "Today's date is " + month + " " + day + ", " + year;
       break;
-    case "whattimeisit":
-      response = new Date().toLocaleTimeString();
-      break;
-    case "stop":
-      response = "Bye!!";
-      toggleBtn(); // stop listening
   }
   return response;
 }
@@ -60,12 +102,13 @@ console.log(getReply("What is my name?")); // "Your name is Benjamin!"
 console.log(getReply("Add fishing to my todo")); // "fishing added to your todo"
 console.log(getReply("Add singing in the shower to my todo")); // "singing in the shower added to your todo"
 console.log(getReply("Remove fishing from my todo")); // "Removed fishing from your todo"
-//console.log(getReply("What is on my todo?"); // "you have 2 todos - fishing and singing in the shower"
-//console.log(getReply("What day is it today?"); // "E.g. if today is 30/8/2019 then it should respond with 30. of August 2019"
-//console.log(getReply("what is 3 + 3?"); // "3 + 3 = 6"
-//console.log(getReply("what is 4 * 12?"); // "4 * 12 = 48"
-//console.log(getReply("Set a timer for 4 minutes"); // "Timer set for 4 minutes" then "The timer is done"
-//console.log(getReply("Extra command"); // "Extra command"
+console.log(getReply("Add making dinner to my todo")); // "fishing added to your todo"
+console.log(getReply("What is on my todo?")); // "you have 2 todos - fishing and singing in the shower"
+console.log(getReply("What day is it today?")); // "E.g. if today is 30/8/2019 then it should respond with 30. of August 2019"
+console.log(getReply("what is 3 + 3?")); // "3 + 3 = 6"
+console.log(getReply("what is 4 * 12?")); // "4 * 12 = 48"
+//console.log(getReply("Set a timer for 1 minutes")); // "Timer set for 4 minutes" then "The timer is done"
+//console.log(getReply("What is today's quote?")); // "Extra command"
 
 
 /*
