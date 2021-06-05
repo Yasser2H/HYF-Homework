ac924263d4b4f0caec1e8c115ee1de4497c919f9
Yasser <Yasser.H@outlook.dk>
2021-06-05 18:09:49 +0200

    Implemented the mentor suggestions.

--- javascript/javascript1/week4/script.js
+++ javascript/javascript1/week4/script.js
@@ -1,12 +1,12 @@
 let personName, nameIsSaved = false,
-  processedText, response, todoList = [];
+  response, todoList = [];
 
 function getReply(command) {
-  processedText = processText(command);
+  let processedText = processText(command);
   //  console.log(processedText);
   if (processedText.substring(0, 13) == "hellomynameis" && !nameIsSaved) {
     nameIsSaved = true;
-    personName = command.substring(17, command.length);
+    personName = command.substring(17);
     return ("Nice to meet you " + personName);
   } else if (processedText.substring(0, 13) == "hellomynameis" && nameIsSaved) {
     return ("You already provided your name! Aren't you " + personName + "?");
@@ -34,8 +34,7 @@ function getReply(command) {
 
 function processText(rawText) {
   let text = rawText.replace(/\s/g, "");
-  text = text.toLowerCase();
-  return text;
+  return text.toLowerCase();
 }
 
 function startTimer(min) {
@@ -57,65 +56,62 @@ function startTimer(min) {
     return (myObj[rndNum].quote);
   }*/
 
-  function processCommand(cleanText) {
-    switch (cleanText) {
-      case "whatismyname?":
-        response = "Your name is " + personName + "!";
-        break;
-      case "whatisonmytodo?": // "you have 2 todos - fishing and singing in the shower"
-        let todoListItems = "";
-        for (var i = 0; i < todoList.length - 1; i++) {
-          todoListItems += todoList[i];
-          todoListItems += " and ";
-        }
+function processCommand(cleanText) {
+  switch (cleanText) {
+    case "whatismyname?":
+      response = "Your name is " + personName + "!";
+      break;
+    case "whatisonmytodo?": // "you have 2 todos - fishing and singing in the shower"
+      let todoListItems = "";
+      for (var i = 0; i < todoList.length - 1; i++) {
         todoListItems += todoList[i];
-        response = "You have " + todoList.length + " items in your todo list - " + todoListItems;
-        break;
-      case "whatdayisittoday?": // "E.g. if today is 30/8/2019 then it should respond with 30. of August 2019"
-        const date = new Date(); // 2009-11-10
-        const year = date.toLocaleString('default', {
-          year: 'numeric'
-        });
-        const month = date.toLocaleString('default', {
-          month: 'long'
-        });
-        const day = date.toLocaleString('default', {
-          day: 'numeric'
-        });
-        response = "Today's date is " + month + " " + day + ", " + year;
-        break;
-    }
-    return response;
+        todoListItems += " and ";
+      }
+      todoListItems += todoList[i];
+      response = "You have " + todoList.length + " items in your todo list - " + todoListItems;
+      break;
+    case "whatdayisittoday?": // "E.g. if today is 30/8/2019 then it should respond with 30. of August 2019"
+      const date = new Date(); // 2009-11-10
+      const year = date.toLocaleString('default', {
+        year: 'numeric'
+      });
+      const month = date.toLocaleString('default', {
+        month: 'long'
+      });
+      const day = date.toLocaleString('default', {
+        day: 'numeric'
+      });
+      response = "Today's date is " + month + " " + day + ", " + year;
+      break;
   }
+  return response;
+}
 
 
 
-  console.log(getReply("Hello my name is Benjamin")); // "Nice to meet you benjamin"
-  console.log(getReply("Hello my name is Benjamin")); // "You already provided your name! Aren't you Benjamin?"
-  console.log(getReply("What is my name?")); // "Your name is Benjamin!"
-  console.log(getReply("Add fishing to my todo")); // "fishing added to your todo"
-  console.log(getReply("Add singing in the shower to my todo")); // "singing in the shower added to your todo"
-  console.log(getReply("Remove fishing from my todo")); // "Removed fishing from your todo"
-  console.log(getReply("Add making dinner to my todo")); // "fishing added to your todo"
-  console.log(getReply("What is on my todo?")); // "you have 2 todos - fishing and singing in the shower"
-  console.log(getReply("What day is it today?")); // "E.g. if today is 30/8/2019 then it should respond with 30. of August 2019"
-  console.log(getReply("what is 3 + 3?")); // "3 + 3 = 6"
-  console.log(getReply("what is 4 * 12?")); // "4 * 12 = 48"
-  //console.log(getReply("Set a timer for 1 minutes")); // "Timer set for 4 minutes" then "The timer is done"
-  //console.log(getReply("What is today's quote?")); // "Extra command"
+console.log(getReply("Hello my name is Benjamin")); // "Nice to meet you benjamin"
+console.log(getReply("Hello my name is Benjamin")); // "You already provided your name! Aren't you Benjamin?"
+console.log(getReply("What is my name?")); // "Your name is Benjamin!"
+console.log(getReply("Add fishing to my todo")); // "fishing added to your todo"
+console.log(getReply("Add singing in the shower to my todo")); // "singing in the shower added to your todo"
+console.log(getReply("Remove fishing from my todo")); // "Removed fishing from your todo"
+console.log(getReply("Add making dinner to my todo")); // "fishing added to your todo"
+console.log(getReply("What is on my todo?")); // "you have 2 todos - fishing and singing in the shower"
+console.log(getReply("What day is it today?")); // "E.g. if today is 30/8/2019 then it should respond with 30. of August 2019"
+console.log(getReply("what is 3 + 3?")); // "3 + 3 = 6"
+console.log(getReply("what is 4 * 12?")); // "4 * 12 = 48"
+//console.log(getReply("Set a timer for 1 minutes")); // "Timer set for 4 minutes" then "The timer is done"
+//console.log(getReply("What is today's quote?")); // "Extra command"
 
 
-  /*
-  - Hello my name is Benjamin - Should save the name benjamin. and respond with "nice to meet you Benjamin". What if someone writes this twice?
-  - What is my name - should respond with the name of the person. What if the name has not yet been mentioned?
-  - Add fishing to my todo - Should respond with "fishing added to your todo". Should add fishing to a list of todos
-  - Add singing in the shower to my todo - Should add singing in the shower to a list of todos
-  - Remove fishing from my todo - Should respond with "Removed fishing from your todo"
-  - What is on my todo? - should respond with the todos. Fx you have 2 todos - fishing and singing in the shower
-  - What day is it today? - Should respond with the date in a human readable format. E.g. if today is 30/8/2019 then it should respond with 30. of August 2019
-  - Should be able to do simple math. fx what is 3 + 3 should respond with 6. Or what is 4 * 12 should respond with 48
-  - Set a timer for 4 minutes - Should respond with "Timer set for 4 minutes". When 4 minutes is up: "Timer done". How do we set a timer in js? Google is your friend here!
-  - Add one or more command to your voice assistant */
-
-
-  //
+/*
+- Hello my name is Benjamin - Should save the name benjamin. and respond with "nice to meet you Benjamin". What if someone writes this twice?
+- What is my name - should respond with the name of the person. What if the name has not yet been mentioned?
+- Add fishing to my todo - Should respond with "fishing added to your todo". Should add fishing to a list of todos
+- Add singing in the shower to my todo - Should add singing in the shower to a list of todos
+- Remove fishing from my todo - Should respond with "Removed fishing from your todo"
+- What is on my todo? - should respond with the todos. Fx you have 2 todos - fishing and singing in the shower
+- What day is it today? - Should respond with the date in a human readable format. E.g. if today is 30/8/2019 then it should respond with 30. of August 2019
+- Should be able to do simple math. fx what is 3 + 3 should respond with 6. Or what is 4 * 12 should respond with 48
+- Set a timer for 4 minutes - Should respond with "Timer set for 4 minutes". When 4 minutes is up: "Timer done". How do we set a timer in js? Google is your friend here!
+- Add one or more command to your voice assistant */
