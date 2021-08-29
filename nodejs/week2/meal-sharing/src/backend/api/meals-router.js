ebef8d463f58fac671f200bf74fa5a87d1f910bd
Yasser <Yasser.H@outlook.dk>
2021-08-29 09:28:18 +0200

    Meal sharing tasks

--- nodejs/week2/meal-sharing/src/backend/api/meals-router.js
+++ nodejs/week2/meal-sharing/src/backend/api/meals-router.js
@@ -4,12 +4,56 @@ const router = express.Router();
 const meals = require("./../data/meals.json");
 
 router.get("/", async (request, response) => {
-  try {
-    console.log(meals);
-    console.log("in /api/meals");
-  } catch (error) {
-    throw error;
-  }
+    try {
+        const reqQuery = request.query;
+
+        if (reqQuery.maxPrice) {
+            if (isNaN(parseInt(reqQuery.maxPrice))) {
+                return response.status(400).json({error: "The input is not a number!"});
+            } else {
+                response.send(meals.filter(meal => meal.price <= reqQuery.maxPrice));
+            }
+        } else if (reqQuery.title) {
+            if (isNaN(parseInt(reqQuery.title))) {
+                return response.status(400).json({error: "The input is not a title!"});
+            } else {
+                response.send(meals.filter(meal => meal.title.toLowerCase().includes(reqQuery.title)));
+            }
+        } else if (reqQuery.createdAfter) {
+            if (isNaN(parseInt(reqQuery.createdAfter))) {
+                return response.status(400).json({error: "The input is not a date!"});
+            } else {
+                response.send(meals.filter(meal => Date.parse(meal.createdAt) >= Date.parse(reqQuery.createdAfter)));
+            }
+        } else if (reqQuery.limit) {
+            if (isNaN(parseInt(reqQuery.limit))) {
+                return response.status(400).json({error: "The input is not a number!"});
+            } else {
+                response.send(meals.slice(0, reqQuery.limit));
+            }
+        } else {
+            response.send(meals);
+        }
+    } catch (error) {
+        throw response.send(400).json(error);
+    }
+});
+
+router.get("/:id", async (request, response) => {
+    if (isNaN(parseInt(request.params.id)))
+        return response.status(400).json({error: "The input is not a number!"});
+    try {
+        const filteredMeals = meals.filter(meals => meals.id === parseInt(request.params.id));
+
+        if (filteredMeals.length === 0) {
+            return response.status(200).json({message: "Meal not found!"});
+        } else {
+            response.send({data: filteredMeals});
+        }
+
+    } catch (error) {
+        throw response.send(400).json(error);
+    }
 });
 
 module.exports = router;
---
+++ nodejs/week2/meal-sharing/src/backend/api/reviews-router.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+
+const reviews = require("./../data/reviews.json");
+
+router.get("/", async (request, response) => {
+    try {
+        response.send({data: reviews});
+    } catch (error) {
+        throw response.send(400).json(error);
+    }
+});
+
+router.get("/:id", async (request, response) => {
+    if (isNaN(parseInt(request.params.id)))
+        return response.status(406).json({error: "The input is not a number!"});
+    try {
+        const filteredReviews = reviews.filter(reviews => reviews.id === parseInt(request.params.id));
+
+        if (filteredReviews.length === 0){
+            return response.status(200).json({message: "Review not found!"});
+        } else {
+            response.send({data: filteredReviews});
+        }
+
+    } catch (error) {
+        throw response.send(400).json(error);
+    }
+});
+
+module.exports = router;
